65c02304a67b7cd737f67ebc60dff05bc29fb43a
Sarahnurulyakin <154663187+Sarahnurulyakin@users.noreply.github.com>
2024-07-05 09:50:47 +0700

    Add files via upload

---
+++ Tugas Mandiri 11/Tugas Mandiri 11/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    class JadwalKerja
+    {
+        public string Nama { get; set; }
+        public string Jam { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        List<List<JadwalKerja>> jadwalKerja = new List<List<JadwalKerja>>();
+
+        for (int i = 0; i < 7; i++)
+        {
+            jadwalKerja.Add(new List<JadwalKerja>());
+        }
+        Console.WriteLine("Nama : Sarah Nurul Yakin");
+        Console.WriteLine("NIM  : 1237050014");
+        Console.WriteLine("|===================================|");
+        Console.WriteLine("|===================================|");
+        Console.WriteLine();
+        while (true)
+        {
+            
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Tambah Shift Kerja");
+            Console.WriteLine("2. Hapus Shift Kerja");
+            Console.WriteLine("3. Tampilkan Jadwal Kerja");
+            Console.WriteLine("4. Keluar");
+            Console.Write("Pilih Opsi: ");
+            int menu = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    TambahShiftKerja(jadwalKerja);
+                    break;
+                case 2:
+                    HapusShiftKerja(jadwalKerja);
+                    break;
+                case 3:
+                    TampilkanJadwalKerja(jadwalKerja);
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Menu tidak valid. Silakan pilih menu yang benar.");
+                    break;
+            }
+        }
+    }
+
+    static void TambahShiftKerja(List<List<JadwalKerja>> jadwal)
+    {
+        Console.WriteLine("\nTambah Shift Kerja:");
+        Console.Write("Masukkan hari (0=Senin, 1=Selasa, 2=rabu, 3=kamis, 4=jumat 5=sabtu, 6=Minggu): ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        if (hari < 0 || hari > 6)
+        {
+            Console.WriteLine("Hari tidak valid.");
+            return;
+        }
+
+        Console.Write("Nama  Karyawan: ");
+        string nama = Console.ReadLine();
+        Console.Write("Jam Kerja     : ");
+        string jam = Console.ReadLine();
+
+        jadwal[hari].Add(new JadwalKerja { Nama = nama, Jam = jam });
+
+        Console.WriteLine("Jam Kerja berhasil ditambahkan.");
+    }
+
+    static void HapusShiftKerja(List<List<JadwalKerja>> jadwal)
+    {
+        Console.WriteLine("\nHapus Shift Kerja:");
+        Console.Write("Masukkan hari (0=Senin, 1=Selasa, ..., 6=Minggu): ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        if (hari < 0 || hari > 6)
+        {
+            Console.WriteLine("Hari tidak valid.");
+            return;
+        }
+
+        Console.Write(" Nama Karyawan shif kerja yang ingin dihapus: ");
+        string nama = Console.ReadLine();
+
+        JadwalKerja jadwalKerja = jadwal[hari].Find(mk => mk.Nama == nama);
+
+        if (jadwalKerja != null)
+        {
+            jadwal[hari].Remove(jadwalKerja);
+            Console.WriteLine("Shift Kerja berhasil dihapus.");
+        }
+        else
+        {
+            Console.WriteLine("Shift Kerja tidak ditemukan.");
+        }
+    }
+
+    static void TampilkanJadwalKerja(List<List<JadwalKerja>> jadwal)
+    {
+        string[] namaHari = { "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu" };
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine($"\n{namaHari[i]}:");
+            if (jadwal[i].Count == 0)
+            {
+                Console.WriteLine("Tidak ada Jadwal Kerja.");
+            }
+            else
+            {
+                foreach (var jadwalKerja in jadwal[i])
+                {
+                    Console.WriteLine($"- {jadwalKerja.Nama} ({jadwalKerja.Jam})");
+                }
+            }
+        }
+    }
+}
---
+++ praktikan pert 14/praktikan pert 14/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Sarah Nurul Yakin", 1000);
+            Console.WriteLine($"Akun {account.Number} dibuat untuk {account.Owner} dengan saldo awal {account.Balance}.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Pembayaran sewa");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(100, DateTime.Now, "Teman membayar hutang");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
+public class Transaction
+{
+    public decimal Amount { get; }
+    public DateTime Date { get; }
+    public string Notes { get; }
+
+    public Transaction(decimal amount, DateTime date, string note)
+    {
+        Amount = amount;
+        Date = date;
+        Notes = note;
+    }
+}
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    private List<Transaction> _allTransactions = new List<Transaction>();
+
+    public string Number { get; private set; }
+    public string Owner { get; set; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+        Owner = name;
+        MakeDeposit(initialBalance, DateTime.Now, "Saldo awal");
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Jumlah deposit harus positif");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Jumlah penarikan harus positif");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Dana tidak cukup untuk penarikan ini");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+    }
+
+    public string GetAccountHistory()
+    {
+        var report = new System.Text.StringBuilder();
+        decimal balance = 0;
+        report.AppendLine("Tanggal\t\tJumlah\tSaldo\tCatatan");
+        foreach (var item in _allTransactions)
+        {
+            balance += item.Amount;
+            report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+        }
+        return report.ToString();
+    }
+}
